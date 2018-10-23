1c23e7525576c4eb45a5d26a8e1ef4f17a79b4ea
steve16351 <cs3sdh@gmail.com>
2018-10-23 11:11:59 +0100

    Handle char type

---
+++ TETL/Converters/CharConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TETL.Converters
+{
+    public class CharConverter<T> : BaseConverter<T, char>, IConvertAndSet
+    {
+        public override void SetValue(object target, string value)
+        {
+            if (value == null) return;
+            if (value.Length < 1) return;
+            _setter((T)target, value[0]);
+        }
+
+        public override string GetValue(object target)
+        {
+            return _getter((T)target).ToString();
+        }
+    }
+
+    public class NullableCharConverter<T> : BaseConverter<T, char?>, IConvertAndSet
+    {
+        public override void SetValue(object target, string value)
+        {
+            if (String.IsNullOrWhiteSpace(value)) return;
+            _setter((T)target, value[0]);
+        }
+
+        public override string GetValue(object target)
+        {
+            return _getter((T)target)?.ToString() ?? String.Empty;
+        }
+    }
+}
--- TETL/Converters/StringConverterFactory.cs
+++ TETL/Converters/StringConverterFactory.cs
@@ -25,6 +25,8 @@ namespace TETL.Converters
             _converters.Add(typeof(Int64?), typeof(NullableInt64Converter<T>));
             _converters.Add(typeof(bool), typeof(BoolConverter<T>));
             _converters.Add(typeof(bool?), typeof(NullableBoolConverter<T>));
+            _converters.Add(typeof(char), typeof(CharConverter<T>));
+            _converters.Add(typeof(char?), typeof(NullableCharConverter<T>));
         }
 
         public static IConvertAndSet Get(PropertyInfo info, TextFileMappingAttribute attribute)
